913ff1af072f6ec7d7cfac444a2f75bc4ef7511d
BrandonWorobi <79031043+BrandonWorobi@users.noreply.github.com>
2021-10-02 14:09:44 -0400

    iniital Commit

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 const Tabs = (topics) => {
   // TASK 3
   // ---------------------
@@ -13,16 +15,48 @@ const Tabs = (topics) => {
   //   <div class="tab">technology</div>
   // </div>
   //
-}
+  const topicsMain = document.createElement('div');
+  topicsMain.classList.add('topics');
+  const array = Array.from(topics.topics);
+  // console.log(array) 
 
-const tabsAppender = (selector) => {
-  // TASK 4
-  // ---------------------
-  // Implement this function which takes a css selector as its only argument.
-  // It should obtain topics from this endpoint: `http://localhost:5000/api/topics` (test it in Postman/HTTPie!).
-  // Find the array of topics inside the response, and create the tabs using the Tabs component.
-  // Append the tabs to the element in the DOM that matches the selector passed to the function.
-  //
-}
+ 
+  const div = array.map((topic) => {
+    const topicCreate = document.createElement('div');
+    topicCreate.classList.add('tab');
+    topicCreate.textContent = topic;
+    return topicCreate;
+  })
 
-export { Tabs, tabsAppender }
+  div.forEach(div => {
+    topicsMain.appendChild(div);
+  })
+
+  return topicsMain;
+
+  // const a = ['it','should','work'];
+  // console.log(Tabs(a))
+
+  const tabsAppender = (selector) => {
+    // TASK 4
+    // ---------------------
+    // Implement this function which takes a css selector as its only argument.
+    // It should obtain topics from this endpoint: `https://lambda-times-api.herokuapp.com/topics`
+    // Find the array of topics inside the response, and create the tabs using the Tabs component.
+    // Append the tabs to the element in the DOM that matches the selector passed to the function.
+    //
+    axios
+      .get('https://lambda-times-api.herokuapp.com/topics')
+      .then(res => {
+        const final = Tabs(res.data);
+        const select = document.querySelector(selector);
+        select.appendChild(final);
+      })
+      .catch(err => {
+        console.log(err)
+      })
+
+  }
+
+  export { Tabs, tabsAppender }
+}
